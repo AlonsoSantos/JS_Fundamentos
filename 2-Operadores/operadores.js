b96e451d83d2547bd84ff8fcda742d4691cdf108
AlonsoSantos <ojimenez34@misena.edu.co>
2019-07-04 11:20:50 -0500

    .

---
+++ 2-Operadores/operadores.js
@@ -0,0 +1,25 @@
+if (1 == '1') {
+  console.log('Si')
+} else {
+  console.log('No')
+}
+
+if (1 === '1'){
+    console.log('Si')
+}else{
+    console.log('No')
+}
+
+var nombre = 'Alonso'
+
+if (nombre == 'alonso'){
+    console.log('Si')
+}else{
+    console.log('No')
+}
+
+if (nombre.toLowerCase() == 'alonso'){
+    console.log('Si')
+}else{
+    console.log('No')
+}
